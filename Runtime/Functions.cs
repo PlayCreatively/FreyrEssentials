d43d35e98039f27ac8a4bf3a000c9fd8c7334b6e
Alexander Freyr Þorgeirsson <lexi-freyr@hotmail.com>
2021-07-19 18:33:36 +0000

    fixed errors

--- Runtime/Functions.cs
+++ Runtime/Functions.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace FreyrEssentials.Unity
 {
-    static class Functions
+    public static class Functions
 	{
-		public static void IgnoreCollision(this UnityEngine.monobeha Collider2D col1, Collider2D col2)
-		=> StaticMono.Get.StartCoroutine(IgnoreCollisionRoutine(col1, col2));
+		public static void IgnoreCollision(this MonoBehaviour mono, Collider2D col1, Collider2D col2)
+		=> mono.StartCoroutine(IgnoreCollisionRoutine(col1, col2));
 		public static IEnumerator IgnoreCollisionRoutine(Collider2D col1, Collider2D col2)
 		{
 			Physics2D.IgnoreCollision(col1, col2, true);
